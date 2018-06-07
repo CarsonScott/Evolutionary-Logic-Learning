f0b3cb81f040c2c1bde0766b6d20556a9aa1a8ca
Carson Scott <carsonjscott14@gmail.com>
2018-06-07 10:48:35 -0400

    added temporal-reinforcement learning

---
+++ src/pattern.py
@@ -0,0 +1,69 @@
+from template import addition, create, generate, express, ExpressionTree
+from goodata import Dict
+from random import randrange as rr
+from lib.relations import *
+from math import tanh
+
+class Proposition:
+	def __init__(self, data):
+		self.data = data
+		self.create()
+	def create(self):
+		data = self.data
+		self.expression = None
+		self.function = None
+		if isinstance(data, str):
+			self.expression = data
+			self.function = generate(data)
+		elif isinstance(data, Proposition):
+			self.expression = express(data)
+			self.function = data
+		self.template = ExpressionTree(self.expression)
+	def __call__(self, input):
+		self.create()
+		return self.function(input)
+
+class Pattern(list):
+	def __init__(self, propositions=[]):
+		for p in propositions:
+			self.append(p)
+	def __call__(self, X):
+		Y = []
+		for f in self:
+			Y.append(f(X))
+		return Y
+	def size(self):
+		return len(self)
+
+class System:
+	def __init__(self, patterns=[]):
+		self.patterns = patterns
+		self.database = Dict()
+		for i in range(len(self.patterns)):
+			self.add(self.patterns[i])
+	def size(self):
+		return len(self.patterns)
+	def add(self, pattern):
+		index = self.size()
+		self.patterns.append(pattern)
+		self.database[index] = Dict()
+	def combine(self, I):
+		P = []
+		for i in I:
+			P = union(P, p)
+		return Pattern(p)
+	def compare(self, i, j):
+		P = intersection(self.patterns[i], self.patterns[j])
+		return Pattern(P)
+
+def random_subset(X, s):
+	Y = []
+	V = []
+	for x in X:
+		V.append(x)
+
+	for i in range(s):
+		j = rr(len(V))
+		Y.append(V[j])
+		del V[i]
+	return Y
--- src/template.py
+++ src/template.py
@@ -3,6 +3,12 @@ from lib.relations import *
 from lib.constraints import *
 from lib.util import *
 
+def to_string(X):
+	string = ''
+	for x in X:
+		string += str(x)
+	return string
+
 def product(X):
 	y = X[0]
 	for i in range(1, len(X)):
@@ -16,11 +22,32 @@ def template(type=None, data=None):
 	return Dict({'type':type, 'data':data})
 
 def association(group=None):
-	output = template('association')
+	output = template('conjunction')
 	output['group'] = group
 	return output
 
-def implication(premise=None, conclusion=None):
+def addition(group=None):
+	output = template('addition')
+	output['group'] = group
+	return output
+def subtraction(group=None):
+	output = template('subtraction')
+	output['group'] = group
+	return output
+def multiplication(group=None):
+	output = template('multiplication')
+	output['group'] = group
+	return output
+def division(group=None):
+	output = template('division')
+	output['group'] = group
+	return output
+
+def implication(group):
+	premise, conclusion = group
+	if conclusion == None and isinstance(premise, tuple):
+		conclusion = premise[0]
+		premise = premise[1]
 	output = template('implication')
 	output['premise'] = premise
 	output['conclusion'] = conclusion
@@ -35,10 +62,10 @@ def convert(template):
 	for i in range(len(data)):
 		if is_template(data[i]):
 			data[i] = convert(data[i])
-	if type == 'association':
-		return Association(data)
-	if type == 'implication':
-		return Implication(data[0], data[1])
+
+	if type in CONNECTIVE_NAMES:
+		index = CONNECTIVE_NAMES.index(type)
+		return CONNECTIVE_FUNCTIONS[index](data)
 
 class Proposition:
 	def __call__(self, memory):
@@ -51,9 +78,9 @@ class Truth(Proposition):
 		return self.variable in memory.keys() and memory[self.variable] == 1
 
 class Implication(Proposition):
-	def __init__(self, premise, conclusion):
-		self.premise = premise
-		self.conclusion = conclusion
+	def __init__(self, pair):
+		self.premise = pair[0]
+		self.conclusion = pair[1]
 	def __call__(self, memory):
 		premise = self.premise
 		if not isinstance(self.premise, Proposition):
@@ -66,7 +93,7 @@ class Implication(Proposition):
 			output = conclusion(memory)
 		return output
 
-class Association(Proposition):
+class Conjunction(Proposition):
 	def __init__(self, group):
 		self.group = group
 	def __call__(self, memory):
@@ -78,6 +105,25 @@ class Association(Proposition):
 			group[i] = data(memory)
 		output = product(group)
 		return bool(output)
+
+class Disjunction(Proposition):
+	def __init__(self, group):
+		self.group = group
+	def __call__(self, memory):
+		group = self.group
+		for i in range(len(group)):
+			data = group[i]
+			if not isinstance(data, Proposition):
+				data = Truth(data)
+			group[i] = data(memory)
+		output = sum(group) > 0
+		return bool(output)
+
+GRAMMAR_SYMBOLS = ['(', ')', ';', ' ']
+CONNECTIVE_SYMBOLS = ['^', '|', '<']
+CONNECTIVE_NAMES = ['conjunction', 'disjunction', 'implication']
+CONNECTIVE_FUNCTIONS = [Conjunction, Disjunction, Implication]
+
 
 def express(proposition, marker=';'):
 	string = '('
@@ -91,15 +137,35 @@ def express(proposition, marker=';'):
 			premise = express(premise, '')
 		if isinstance(conclusion, Proposition):
 			conclusion = express(conclusion, '')
-		string += str(premise) + ' / ' + str(conclusion)
 
-	elif isinstance(proposition, Association):
+		index = CONNECTIVES.index(Implication)
+		symbol = CONNECTIVE_SYMBOLS[index]
+		string += str(premise) + ' ' + symbol + ' ' + str(conclusion)
+
+	elif isinstance(proposition, Conjunction):
 		group = proposition.group
 		if isinstance(group[0], Proposition):
 			group[0] = express(group[0], '')
 		string += str(group[0])
+
+		index = CONNECTIVES.index(Conjunction)
+		symbol = CONNECTIVE_SYMBOLS[index]
 		for i in range(1, len(group)):
-			string += ' ^ '
+			string += ' ' + symbol + ' '
+			data = group[i]
+			if isinstance(data, Proposition):
+				data = express(data, '')
+			string += str(data)
+
+	elif isinstance(proposition, Disjunction):
+		group = proposition.group
+		if isinstance(group[0], Proposition):
+			group[0] = express(group[0], '')
+		string += str(group[0])
+		index = CONNECTIVES.index(Disjunction)
+		symbol = CONNECTIVE_SYMBOLS[index]
+		for i in range(1, len(group)):
+			string += ' ' + symbol + ' '
 			data = group[i]
 			if isinstance(data, Proposition):
 				data = express(data, '')
@@ -114,9 +180,11 @@ class EmbeddedList(list):
 		self[index] = EmbeddedList(1, self[index])
 
 class ExpressionTree(EmbeddedList):
-	def __init__(self):
+	def __init__(self, expression=None):
 		super().__init__(0)
 		self.state = 0
+		if expression != None:
+			self.parse(expression)
 
 	def expand(self, index):
 		self[index] = ExpressionTree()
@@ -154,6 +222,22 @@ class ExpressionTree(EmbeddedList):
 				if y != None:self.state = 0
 
 	def parse(self, expression):
+		previous = None
+		elements = []
+		for i in range(len(expression)):
+			character = expression[i]
+			if character not in CONNECTIVE_SYMBOLS and character not in GRAMMAR_SYMBOLS:
+				if previous == None:
+					previous = str()
+				previous += character
+
+			elif character != ' ':
+				if previous != None:
+					elements.append(previous)
+				elements.append(character)
+				previous = None
+		expression = elements
+		
 		for i in range(len(expression)):
 			character = expression[i]
 			self.update(character)
@@ -165,12 +249,11 @@ def encode(tree):
 			tree[i] = encode(tree[i])
 	if len(tree) == 1:
 		return tree[0]
-	else:
+	elif len(tree) == 3:
 		a,r,b = tree
-		if r == '^':
-			r = 'association'
-		if r == '/':
-			r = 'implication'
+		if r in CONNECTIVE_SYMBOLS:
+			index = CONNECTIVE_SYMBOLS.index(r)
+			r = CONNECTIVE_NAMES[index]
 		x = [a, b]
 		return template(r, x)
 
@@ -180,3 +263,156 @@ def generate(expression):
 	template = encode(tree)
 	function = convert(template)
 	return function
+
+def variables(expression):
+	tree = ExpressionTree()
+	tree.parse(expression)
+	print(tree)
+
+class ProbabilitySpace(Dict):
+	def __init__(self, keys):
+		self.examples = 0
+		for i in range(len(keys)):
+			k = keys[i]
+			self[k] = 0
+
+	def train(self, keys):
+		for i in range(len(keys)):
+			k = keys[i]
+			self[k] += 1
+		self.examples += 1
+
+	def compute(self):
+		size = self.examples
+		distribution = dict(self)
+		for i in distribution.keys():
+			distribution[i] /= size
+		return distribution
+
+	def reset(self):
+		self.examples = 0
+		for i in self.keys():
+			self[i] = 0
+
+class RelationalMatrix(Dict):
+	def __init__(self, keys):
+		self.examples = 0
+		for i in range(len(keys)):
+			ki = keys[i]
+			self[ki] = Dict()
+			for j in range(i, len(keys)):
+				kj = keys[j]
+				self[ki][kj] = 0
+		
+	def train(self, keys):
+		for i in range(len(keys)):
+			ki = keys[i]
+			for j in range(i, len(keys)):
+				kj = keys[j]
+				self[ki][kj] += 1
+		self.examples += 1
+	def compute(self):
+		size = self.examples
+		distribution = Dict(self)
+		keys = self.keys()
+		for i in range(len(keys)):
+			for j in range(i, len(keys)):
+				ki = keys[i]
+				kj = keys[j]
+				distribution[ki][kj] /= size
+		return distribution
+	def reset(self):
+		self.examples = 0
+		for i in range(len(keys)):
+			for j in range(len(keys)):
+				ki = keys[i]
+				kj = keys[j]
+				self[ki][kj] = 0
+
+class Group(Dict):
+	def __init__(self, keys):
+		self.prob_space = ProbabilitySpace(keys)
+		self.rel_matrix = RelationalMatrix(keys)
+		self.distribution = None
+		self.associations = None 
+
+	def train(self, keys):
+		self.prob_space.train(keys)
+		self.rel_matrix.train(keys)
+	def compute(self):
+		self.distribution = self.prob_space.compute()
+		self.associations = self.rel_matrix.compute()
+
+	def get(self, initial, final):
+		y = Dict()
+		y['x'] = self.distribution[initial]
+		y['y'] = self.distribution[final]
+		y['r'] = self.associations[initial][final]
+		return y
+		
+
+keys = ['a', 'b', 'c', 'd'] 
+space = ProbabilitySpace(keys)
+joint = RelationalMatrix(keys)
+
+group = Group(keys)
+
+X = [['a', 'b', 'c'],
+	 ['a', 'c'],
+	 ['b', 'd']]
+
+c = 0
+for i in range(50):
+	x = X[c]
+	c += 1
+	if c >= len(X): 
+		c = 0
+	group.train(x)
+
+group.compute()
+print(group.get('a', 'b'))
+
+memory = {'a':0, 'b':1, 'c':0,'d':1}
+expression = '((a | b) < (c ^ d));'
+function = generate(expression)
+output = function(memory)
+print(output)
+
+
+def train(inputs, outputs, rules, examples):
+	scores = Dict()
+	for i in outputs:
+		scores[i] = 0
+
+	for i in range(len(examples)):
+		values = examples[i]
+		total = sum(list(values.values()))
+
+		for j in range(len(rules)):
+			function = convert(rules[j])
+			score = function(values)
+			index = outputs[j]
+			scores[j] += score * 1/len(examples)
+	return scores
+
+
+rules = []
+
+inputs  = [0,1,2]
+outputs = []
+
+examples = []
+for i in range(10):
+	example = Dict()
+	for j in range(len(inputs)):
+		example[j] = 0
+	for j in range(2):
+		example[rr(len(example))] = 1
+	examples.append(example)
+
+	active = [rr(len(inputs)) for j in range(2)]
+	rules.append(create('conjunction', active))
+	outputs.append(i)
+
+y = train(inputs, outputs, rules, examples)
+print(y)
