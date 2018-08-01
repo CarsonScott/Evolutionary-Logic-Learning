4b1eb9bd1952432d659546118a4ad9bd35524ce0
Carson Scott <carsonjscott14@gmail.com>
2018-08-01 09:23:59 -0400

    cleaned up functional topology code

--- src/functional_topology.py
+++ src/functional_topology.py
@@ -1,5 +1,5 @@
 from topology import *
-from settheory import *
+from lib.relations import *
 
 def shuffle(x):
 	y = []
@@ -20,11 +20,39 @@ def reorder(x, k):
 		y.append(x[k[i]])
 	return y
 
+def display(data, indent=''):
+	string = ''
+	if isinstance(data, dict):
+		string = '\n'
+		for i in data.keys():
+			x = data[i]
+			s = indent + str(i) + ': '
+			if isinstance(x, dict) or isinstance(x, list):
+				s += display(x, '	')
+			else:
+				s += ' '
+				s += str(x)
+			s += '\n'
+			string += s
+	elif isinstance(data, list):
+		s = []
+		for x in data:
+			s.append(str(x))
+		string = indent + '{' + merge(',', s) + '}'
+	if indent == '':
+		print(string)
+	else:return string
+
 class FunctionalTopology(Dictionary):
+	
 	def __init__(self, inputs, history_size, schedule_size, functions=None, weights=None, thresholds=None):
 		for i in range(len(inputs)):
 			k = str(i)
 			x = inputs[i]
+			if x != None:
+				for j in range(len(x)):
+					x[j] = str(x[j])
+
 			if functions == None:
 				s = None
 			else:s = functions[i]
@@ -37,6 +65,7 @@ class FunctionalTopology(Dictionary):
 			self.set(k,s,x,w,t)
 		self.init(history_size, schedule_size)
 			
+	
 	def keys(self):
 		keys = []
 		for i in super().keys():
@@ -44,101 +73,142 @@ class FunctionalTopology(Dictionary):
 				keys.append(i)
 		return keys
 
-	def get_values(self, keys):
-		values = []
-		for i in keys:
-			values.append(self[i])
-		return values
-
+	
 	def init(self, history_size, schedule_size):
-		history = [None for i in range(history_size)]
+		history = []
 		self.set_dependent('', 'history', history)
+		self.set_dependent('', 'capacity', schedule_size)
 		schedule = []
 		self.set_dependent('', 'schedule', schedule)
-		interface = self.compute_interface()
+		interface = self.set_interface()
 		self.set_dependent('', 'interface', interface)
 		for i in self.keys():
-			targets = self.compute_targets(i)
+			targets = self.set_targets(i)
 			self.set_dependent(i, 'targets', targets)
+	
+	
 	def set(self, key, function=None, inputs=None, weights=None, threshold=None):
 		self[key] = unknown
 		self.set_function(key, function)
 		self.set_inputs(key, inputs)
 		self.set_weights(key,  weights)
 		self.set_threshold(key, threshold)
+	
+	def get_dependent(self, key, ext):
+		i = merge('.', [str(key), str(ext)])
+		return self[i]
 	def set_dependent(self, key, ext, val=None):
 		i = merge('.', [str(key), str(ext)])
 		self[i] = val
+	
+	def get_function(self, key):
+		return self.get_dependent(key, 'function')
 	def set_function(self, key, function):
 		self.set_dependent(key, 'function', function)
+	def execute_function(self, function, values):
+		try:return function(values)
+		except:return unknown
+
+	def get_inputs(self, key):
+		return self.get_dependent(key, 'inputs')
 	def set_inputs(self, key, inputs):
 		self.set_dependent(key, 'inputs', inputs)
+
+	def get_weights(self, key):
+		return self.get_dependent(key, 'weights')
 	def set_weights(self, key, weights):
 		self.set_dependent(key, 'weights', weights)
+
+	def get_capacity(self):
+		return self.get_dependent('', 'capacity')
+	def set_capacity(self, capacity):
+		self.set_dependent('', 'capacity', capacity)
+
+	def get_threshold(self, key):
+		return self.get_dependent('', 'threshold')
 	def set_threshold(self, key, threshold):
 		self.set_dependent(key, 'threshold', threshold)
-	def get_function(self, key):
-		return self[key + '.function']
-	def get_inputs(self, key):
-		return self[key + '.inputs']
-	def get_weights(self, key):
-		return self[key + '.weights']
-	def get_threshold(self, key):
-		return self[key + '.threshold']
+	
 	def get_targets(self, key):
 		return self[key + '.targets']
-	def get_interface(self):
-		return self['.interface']
-	def get_history(self):
-		return self['.history']
-	def get_schedule(self):
-		return self['.schedule']
-	def compute_interface(self):
-		roots = self.keys()
-		leaves = list()
-		for i in self.keys():
-			x = self.get_inputs(i)
-			if x == None:
-				leaves.append(i)
-			else:
-				for j in x:
-					if j in roots:
-						del roots[roots.index(j)]
-		return leaves, roots
-	def compute_targets(self, key):
+	def set_targets(self, key):
 		targets = []
 		for i in self.keys():
 			inputs = self.get_inputs(i)
 			if inputs != None and key in inputs:
 				targets.append(i)
 		return targets
+	
+	def get_interface(self):
+		return self['.interface']
+	def set_interface(self):
+		roots = self.keys()
+		leaves = list()
+		for i in self.keys():
+			x = self.get_inputs(i)
+			if x == None:leaves.append(i)
+			else:
+				for j in x:
+					if j in roots:
+						del roots[roots.index(j)]
+		return leaves, roots
 
-	def set_values(self, keys, values):
-		j = 0
-		for i in keys:
-			self[i] = values[j]
-			j += 1 
-
-	def update_leaves(self, values):
-		inputs = self.get_interface()[0]
-		self.set_values(inputs, values)
-	def update_history(self, key):
+	def get_history(self):
+		return self['.history']
+	def set_history(self, key):
 		self['.history'].append(key)
-		old = self['.history'][0]
-		del self['.history'][0]
-		self[old] = unknown
-
+		history = self.get_history()
+		capacity = self.get_capacity()
+		if len(history) > capacity:
+			difference = len(history) - capacity
+			self['.history'] = history[difference:len(history)]
+			for i in range(difference):
+				k = history[i]
+				self[k] = unknown
+	
+	def get_schedule(self):
+		return self['.schedule']
 	def set_schedule(self, key):
 		if isinstance(key, list):
 			for k in key:self.set_schedule(k)
 		else:self['.schedule'].append(key)
-	def del_schedule(self, key):
-		del self['.schedule'][self['.schedule'].index(key)]
+	def update_schedule(self, key):
+		schedule =  self.get_schedule()
+		if key in schedule:
+			index = schedule.index(key)
+			del schedule[index]
 
-	def execute(self, function, values):
-		try:
-			return function(values)
-		except:return unknown
+	
+	def get_value(self, key):
+		return self[key]
+	def set_value(self, key, value):
+		self[key] = value
+	
+	def get_values(self, keys):
+		values = []
+		for i in keys:
+			values.append(self[i])
+		return values
+	def set_values(self, keys, values):
+		j = 0
+		for i in range(len(keys)):
+			k = keys[i]
+			v = values[i]
+			self[k] = v
+
+	def get_leaves(self):
+		return self.get_interface()[0]
+	def get_roots(self):
+		return self.get_interface()[1]
+	def get_recent(self):
+		return self.get_history()[0]
+
+	def inputs(self, values):
+		leaves = self.get_leaves()
+		self.set_values(leaves, values)
+	def outputs(self):
+		roots = self.get_roots()
+		return self.get_values(roots)	
 
 	def compute(self, key):
 		sources = self.get_inputs(key)
@@ -146,16 +216,16 @@ class FunctionalTopology(Dictionary):
 			inputs = self.get_values(sources)
 			if unknown not in inputs:
 				function = self.get_function(key)
-				output = self.execute(function, inputs)
-				self.update_history(key)
+				output = self.execute_function(function, inputs)
+				self.set_history(key)
 			else:output = unknown
 			self[key] = output
 		return self[key]
-
-	def select(self, targets):
+	
+	def select(self, paths):
 		options = []
 		scores = []
-		for i in targets:
+		for i in paths:
 			if self[i] == unknown:
 				sources = self.get_inputs(i)
 				inputs = self.get_values(sources)
@@ -173,8 +243,7 @@ class FunctionalTopology(Dictionary):
 		options = reorder(options, indices)
 		return options
 
-	def __call__(self):
-		current = None
+	def update(self):
 		schedule = self.get_schedule()
 		if len(schedule) > 0:
 			done = False
@@ -185,19 +254,19 @@ class FunctionalTopology(Dictionary):
 					current = schedule[0]
 					if current != None:
 						done = True
-						self.compute(current)
-					self.del_schedule(current)
+						output = self.compute(current)
+					self.update_schedule(current)
 		else:
 			history = self.get_history()
 			if len(history) > 0 and history[len(history)-1] != None:
-				recent = history[len(history)-1]
+				recent = self.get_recent()
 				if recent != None:
 					targets = self.get_targets(recent)
 					if targets != None:
 						selection = self.select(targets)
 						self.set_schedule(selection)
 			else:
-				leaves = self.get_interface()[0]
+				leaves = self.get_leaves()
 				targets = list()
 				for i in leaves:
 					targets = union(targets, self.get_targets(i))
@@ -205,7 +274,13 @@ class FunctionalTopology(Dictionary):
 					selection = self.select(targets)
 					self.set_schedule(selection)
 
-		roots = self.get_interface()[1]
+	def describe(self):		
+		roots = self.get_roots()
 		outputs = self.get_values(roots)
-		if unknown not in outputs:
-			return outputs
+		history = self.get_history()
+		schedule = self.get_schedule()
+		description = Dictionary()
+		description['schedule'] = schedule
+		description['history'] = history
+		description['output'] = outputs
+		return description
